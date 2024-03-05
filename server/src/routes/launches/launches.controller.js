e297bdd06857de4f03853ef6e12743466c233433
VENDABLEPATH <wianjacobs04@gmail.com>
2024-03-05 14:19:48 +0200

    Added deployment and build scripting, and /launches router, controller and model

---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,10 @@
+const { launches } = require('../../models/launches.model');
+
+function getLaunches(req, res){
+    return res.status(200).json(launches);
+};
+
+module.exports = {
+    getLaunches
+}
+
---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const launchesRouter = express.Router();
+
+const getLaunches = require('./launches.controller');
+
+launchesRouter.get('/launches', getLaunches);
+
+module.exports = launchesRouter;
+
